b9b3d2cea7bc2b83ecc30bfc8205623f191d17fc
Adeyinka Giwa <yinkadev1992@gmail.com>
2022-10-30 14:07:15 +0000

    test for post started

---
+++ backend/routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware(['auth:sanctum'])->group( function () {
+    Route::resource('/post', PostController::class);
+    Route::resource('/user', UserController::class)->except(['create', 'store', 'show']);
+});
+
+Route::prefix('/user')->group(function () {
+    Route::post('/', [UserController::class, 'store']);
+    Route::post('/login', [UserController::class, 'show']);
+});
+
---
+++ backend/tests/Unit/PostToTimelineTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Unit;
+
+use Database\Factories\UserFactory;
+use Illuminate\Support\Facades\Auth;
+use Tests\TestCase;
+use App\Models\User;
+
+class PostToTimelineTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_post_can_be_created()
+    {
+        $this->withExceptionHandling();
+
+        $response =  $this->post( 'http://127.0.0.1:8000/api/post?id=34',[], [
+            'Authorization'=> "Bearer 1|oy1YFUJUxEy0YMYj6vmnJZK5AsWEfZi28sfXMid1"
+        ]);
+
+        $this->assertEquals(201, $response->status() );
+
+    }
+}
